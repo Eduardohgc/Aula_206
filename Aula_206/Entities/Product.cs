bcfa745eb2c767394a47b53019fdaf3533d36ea3
Eduardo Henrique <eduardo.h.guerra@hotmail.com>
2020-04-19 17:35:43 -0300

    Projeto sem funcao nenhuma, apenas implementacao da interface em Product

---
+++ Aula_206/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Aula_206.Entities
+{
+    class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Prince { get; set; }
+
+        public Product(string name, double prince)
+        {
+            Name = name;
+            Prince = prince;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Prince.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Product other)
+        {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+    }
+}
---
+++ Aula_206/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Aula_206.Entities;
+
+namespace Aula_206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort();
+
+            foreach (Product item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
