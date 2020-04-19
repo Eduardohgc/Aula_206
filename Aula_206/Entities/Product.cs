5712860f1bd88f9b4c021c76d9934ca47ab68e80
Eduardo Henrique <eduardo.h.guerra@hotmail.com>
2020-04-19 17:42:52 -0300

    Projeto listando por ordem de preco

--- Aula_206/Entities/Product.cs
+++ Aula_206/Entities/Product.cs
@@ -7,22 +7,22 @@ namespace Aula_206.Entities
     class Product : IComparable<Product>
     {
         public string Name { get; set; }
-        public double Prince { get; set; }
+        public double Price { get; set; }
 
-        public Product(string name, double prince)
+        public Product(string name, double price)
         {
             Name = name;
-            Prince = prince;
+            Price = price;
         }
 
         public override string ToString()
         {
-            return Name + ", " + Prince.ToString("F2", CultureInfo.InvariantCulture);
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
         }
 
         public int CompareTo(Product other)
         {
-            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+            return Price.CompareTo(other.Price);
         }
     }
 }
--- Aula_206/Program.cs
+++ Aula_206/Program.cs
@@ -7,6 +7,13 @@ namespace Aula_206
 {
     class Program
     {
+        /*
+            Na forma anterior nossa classe Product não fica fechada
+            para alteração: se o critério de comparação mudar,
+            precisaremos alterar a classe Product. Ou seja se a classe 
+            quiser modificar a comparação pelo Price em ver pelo Name a classe terá de ser
+            modificada.
+        */
         static void Main(string[] args)
         {
             List<Product> list = new List<Product>();
